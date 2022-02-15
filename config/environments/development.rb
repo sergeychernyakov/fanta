e0c3fa1d30ec2337eab97b42ebbbb698685461af
Shahid Nawaz <shahidjutt72@yahoo.com>
2022-02-15 23:48:58 +0500

    sign in, sign up, confirmation, email, reset password,integrated using devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: "localhost:#{ENV.fetch('PORT', 3000)}" }
+  config.action_mailer.delivery_method = :letter_opener
 end
