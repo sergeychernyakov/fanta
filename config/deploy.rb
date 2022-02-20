f029c9f51ca8d96be8296c09330486c65f2de08e
Azhar2k16 <azharkalam05@gmail.com>
2022-02-20 15:36:14 +0500

    ssh keys added

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,3 @@
-before "deploy:assets:precompile", "deploy:yarn_install"
-namespace :deploy do
-  desc "Run rake yarn install"
-  task :yarn_install do
-    on roles(:web) do
-      within release_path do
-        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
-      end
-    end
-  end
-end
 lock "~> 3.16.0"
 
 require 'capistrano-db-tasks'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '3.92.198.224', user: 'deploy', roles: %w{web app db}
+server '3.236.201.250', user: 'deploy', roles: %w{web app db}
 set :rails_env, 'production'
 set :ssh_options, { 
   forward_agent: true, 
