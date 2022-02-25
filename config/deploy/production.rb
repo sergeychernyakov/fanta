509e4cbcf8fc9cf285495f7c1aba1b5fe8c80ea1
Azhar2k16 <azharkalam05@gmail.com>
2022-02-26 01:13:52 +0500

    prod.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -44,11 +44,10 @@ set :rails_env, 'production'
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/user_name/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+ set :ssh_options, {
+   keys: %w(/home/deploy/.ssh/id_rsa),
+   forward_agent: false,
+ }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
